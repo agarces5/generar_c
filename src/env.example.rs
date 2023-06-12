99ca89bd31eae90f3be9dea2242d5dcb970db635
Antonio Garcés <garces080995@gmail.com>
2023-06-12 20:30:06 +0200

    Add example env file

---
+++ src/env.example.rs
@@ -0,0 +1,11 @@
+pub const SERVIDOR: &str = "server.example.com";
+pub const USUARIO: &str = "admin";
+pub const CLAVE: &str = "1234";
+pub const RUTA: &str = r"\\srv-c-files\C-FILES";
+pub const EJECUTABLE: &str = r"\\srv-exes\programs";
+pub const HOTELES: &str = r#"{
+	"0001": "hotel1",
+	"0002": "hotel2",
+	"0003": "hotel3",
+	"0004": "hotel4",
+}"#;
